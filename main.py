5fbfe98b242cd373f59933511d7dcf9361d75345
Student <mishamityurin@gmail.com>
2021-09-22 21:14:17 +0300

    add new code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ data = dict()
 number = int(input('Сколько стран: '))
 
 for counter in range(number):
-name = input('Название страны: ')
+    name = input('Название страны: ')
 population = int(input('Население: '))
 data[name] = population
 
 sorted_data = sorted(data.items(), key=lambda x: x[1])
 for name, population in sorted_data[::-1]:
-print(name, population)
+    print(name, population)
