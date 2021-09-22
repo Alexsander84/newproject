d0105f479c33e661f60782c36ab7604dd02a4d55
Student <mishamityurin@gmail.com>
2021-09-22 21:12:21 +0300

    new 2

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello world!");
-print("Hello world!");
+data = dict()
+number = int(input('Сколько стран: '))
+
+for counter in range(number):
+name = input('Название страны: ')
+population = int(input('Население: '))
+data[name] = population
+
+sorted_data = sorted(data.items(), key=lambda x: x[1])
+for name, population in sorted_data[::-1]:
+print(name, population)
